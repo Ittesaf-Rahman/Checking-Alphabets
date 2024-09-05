3dec324b3a9819ab41a140f227f249b1b80934d5
Ittesaf-Rahman <ittesafrahman2011@gmail.com>
2024-09-05 21:26:21 +0600

    Add files via upload

---
+++ Checking Alphabets.py
@@ -0,0 +1,97 @@
+Alphabet = input("Enter a alphabet latter = ")
+if Alphabet == 'a':
+    print(Alphabet, "Lower case letters here. Its capital letters A and its cursive form is 𝓪")
+elif Alphabet == 'A':
+    print(Alphabet, "Upper case letters here. Its small letters a and its cursive form is 𝓐")
+elif Alphabet == 'b':
+    print(Alphabet, "Lower case letters here. Its capital letters B and its cursive form is 𝓫")
+elif Alphabet == 'B':
+    print(Alphabet, "Upper case letters here. Its small letters b and its cursive form is 𝓑")
+elif Alphabet == 'c':
+    print(Alphabet, "Lower case letters here. Its capital letters C and its cursive form is 𝓬")
+elif Alphabet == 'C':
+    print(Alphabet, "Upper case letters here. Its small letters c and its cursive form is 𝓒")
+elif Alphabet == 'd':
+    print(Alphabet, "Lower case letters here. Its capital letters D and its cursive form is 𝓭")
+elif Alphabet == 'D':
+    print(Alphabet, "Upper case letters here. Its small letters d and its cursive form is 𝓓")
+elif Alphabet == 'e':
+    print(Alphabet, "Lower case letters here. Its capital letters E and its cursive form is 𝓮")
+elif Alphabet == 'E':
+    print(Alphabet, "Upper case letters here. Its small letters e and its cursive form is 𝓔")
+elif Alphabet == 'f':
+    print(Alphabet, "Lower case letters here. Its capital letters F and its cursive form is 𝓯")
+elif Alphabet == 'F':
+    print(Alphabet, "Upper case letters here. Its small letters f and its cursive form is 𝓕")
+elif Alphabet == 'g':
+    print(Alphabet, "Lower case letters here. Its capital letters G and its cursive form is 𝓰")
+elif Alphabet == 'G':
+    print(Alphabet, "Upper case letters here. Its small letters g and its cursive form is 𝓖")
+elif Alphabet == 'h':
+    print(Alphabet, "Lower case letters here. Its capital letters H and its cursive form is 𝓱")
+elif Alphabet == 'H':
+    print(Alphabet, "Upper case letters here. Its small letters h and its cursive form is 𝓗")
+elif Alphabet == 'i':
+    print(Alphabet, "Lower case letters here. Its capital letters I and its cursive form is 𝓲")
+elif Alphabet == 'I':
+    print(Alphabet, "Upper case letters here. Its small letters i and its cursive form is 𝓘")
+elif Alphabet == 'j':
+    print(Alphabet, "Lower case letters here. Its capital letters J and its cursive form is 𝓳")
+elif Alphabet == 'J':
+    print(Alphabet, "Upper case letters here. Its small letters j and its cursive form is 𝓙")
+elif Alphabet == 'k':
+    print(Alphabet, "Lower case letters here. Its capital letters K and its cursive form is 𝓴")
+elif Alphabet == 'K':
+    print(Alphabet, "Upper case letters here. Its small letters k and its cursive form is 𝓚")
+elif Alphabet == 'l':
+    print(Alphabet, "Lower case letters here. Its capital letters L and its cursive form is 𝓵")
+elif Alphabet == 'L':
+    print(Alphabet, "Upper case letters here. Its small letters l and its cursive form is 𝓛")
+elif Alphabet == 'm':
+    print(Alphabet, "Lower case letters here. Its capital letters M and its cursive form is 𝓶")
+elif Alphabet == 'M':
+    print(Alphabet, "Upper case letters here. Its small letters m and its cursive form is 𝓜")
+elif Alphabet == 'n':
+    print(Alphabet, "Lower case letters here. Its capital letters N and its cursive form is 𝓷")
+elif Alphabet == 'N':
+    print(Alphabet, "Upper case letters here. Its small letters n and its cursive form is 𝓝")
+elif Alphabet == 'o':
+    print(Alphabet, "Lower case letters here. Its capital letters O and its cursive form is 𝓸")
+elif Alphabet == 'O':
+    print(Alphabet, "Upper case letters here. Its small letters o and its cursive form is 𝓞")
+elif Alphabet == 'p':
+    print(Alphabet, "Lower case letters here. Its capital letters P and its cursive form is 𝓹")
+elif Alphabet == 'P':
+    print(Alphabet, "Upper case letters here. Its small letters p and its cursive form is 𝓟")
+elif Alphabet == 'q':
+    print(Alphabet, "Lower case letters here. Its capital letters Q and its cursive form is 𝓺")
+elif Alphabet == 'Q':
+    print(Alphabet, "Upper case letters here. Its small letters q and its cursive form is 𝓠")
+elif Alphabet == 'r':
+    print(Alphabet, "Lower case letters here. Its capital letters R and its cursive form is 𝓻")
+elif Alphabet == 'R':
+    print(Alphabet, "Upper case letters here. Its small letters r and its cursive form is 𝓡")
+elif Alphabet == 's':
+    print(Alphabet, "Lower case letters here. Its capital letters S and its cursive form is 𝓼")
+elif Alphabet == 'S':
+    print(Alphabet, "Upper case letters here. Its small letters s and its cursive form is 𝓢")
+elif Alphabet == 't':
+    print(Alphabet, "Lower case letters here. Its capital letters T and its cursive form is 𝓽")
+elif Alphabet == 'T':
+    print(Alphabet, "Upper case letters here. Its small letters t and its cursive form is 𝓣")
+elif Alphabet == 'w':
+    print(Alphabet, "Lower case letters here. Its capital letters W and its cursive form is 𝔀")
+elif Alphabet == 'W':
+    print(Alphabet, "Upper case letters here. Its small letters w and its cursive form is 𝓦")
+elif Alphabet == 'x':
+    print(Alphabet, "Lower case letters here. Its capital letters X and its cursive form is 𝔁")
+elif Alphabet == 'X':
+    print(Alphabet, "Upper case letters here. Its small letters x and its cursive form is 𝓧")
+elif Alphabet == 'y':
+    print(Alphabet, "Lower case letters here. Its capital letters Y and its cursive form is 𝔂")
+elif Alphabet == 'Y':
+    print(Alphabet, "Upper case letters here. Its small letters y and its cursive form is 𝓨")
+elif Alphabet == 'z':
+    print(Alphabet, "Lower case letters here. Its capital letters Z and its cursive form is 𝔃")
+elif Alphabet == 'Z':
+    print(Alphabet, "Upper case letters here. Its small letters z and its cursive form is 𝓩")
